65905ceca6d8d01e14f1007717627cb0f7c58311
Eduardo Antuña Díez <eduadiez@gmail.com>
2024-05-15 17:12:46 +0200

    add no_std, no_main, no_mangle and panic_handler to be able to compile

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[no_mangle]
+fn main() {}
+
+#[panic_handler]
+fn on_panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
